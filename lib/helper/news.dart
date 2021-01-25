849fad13f2d46701540a408e3b44754d8c701325
Mustafa <18701031@mersin.edu.tr>
2021-01-25 23:44:56 +0300

    File Added

---
+++ lib/helper/news.dart
@@ -0,0 +1,7 @@
+import 'package:flutternews/models/article_model.dart';
+
+class News{
+  List<ArticleModel> news = [];
+  String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
+
+}

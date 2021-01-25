c3c1d069e59470cb4e5098c124a07750a4d38503
Mustafa <18701031@mersin.edu.tr>
2021-01-25 23:44:29 +0300

    File Added

---
+++ lib/views/category_news.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CategoryNews extends StatefulWidget {
+  @override
+  _CategoryNewsState createState() => _CategoryNewsState();
+}
+
+class _CategoryNewsState extends State<CategoryNews> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

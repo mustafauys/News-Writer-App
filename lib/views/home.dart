cafb066117c3493f995c527d9ec95a9a98a352d7
Mustafa <18701031@mersin.edu.tr>
2021-01-25 23:44:17 +0300

    Menu Bar Created

---
+++ lib/views/home.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:flutternews/helper/data.dart';
+import 'package:flutternews/models/category_models.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  List<CategoryModel> categories = new List<CategoryModel>();
+  @override
+  void initState() {
+    super.initState();
+    categories = getCategories();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Flutter"),
+            Text("News", style: TextStyle(color: Colors.blue)),
+          ],
+        ),
+        centerTitle: true,
+        elevation: 0.0,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                height: 70,
+                child: ListView.builder(
+                    itemCount: categories.length,
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (context, index) {
+                      return CategoryTile(
+                        imageUrl: categories[index].imageUrl,
+                        categoryName: categories[index].categoryName,
+                      );
+                    }))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+  final imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){
+
+      },
+          child: Container(
+        margin: EdgeInsets.only(right: 16),
+        child: Stack(
+          children: [
+            ClipRRect(
+                borderRadius: BorderRadius.circular(6),
+                child: Image.network(
+                  imageUrl,
+                  width: 120,
+                  height: 60,
+                  fit: BoxFit.cover,
+                )),
+            Container(
+              alignment: Alignment.center,
+              width: 120,
+              height: 60,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(6),
+                color: Colors.black26,
+              ),
+              child: Text(
+                categoryName,
+                style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w500),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+
+  final String imageUrl, title, desc;
+  BlogTile({@required this.imageUrl, @required this.title, @required this.desc});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Image.network(imageUrl),
+          Text(title),
+          Text(desc),
+        ],
+      ),
+    );
+  }
+}

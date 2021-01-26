b5b415f1820a1a118cabbdbbf00b51ba2eb4e4fb
Mustafa <18701031@mersin.edu.tr>
2021-01-26 23:53:15 +0300

    Menu Bar Data Processed

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -1,7 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutternews/models/article_model.dart';
+import 'package:http/http.dart' as http;
 
-class News{
+class News {
   List<ArticleModel> news = [];
-  String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
+  Future<void> getNews() async {
+    String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
 
+  var response = await http.get(url);
+  var jsonData = jsonDecode(response.body);
+  if(jsonData['status'] == "ok") {
+    jsonData["articles"].forEach((element){
+
+      if(element["urlToImage"] != null && element['description'] !=null) {
+        ArticleModel articleModel = ArticleModel(
+          title: element['title'],
+          author: element["author"],
+          description: element["description"],
+          url: element["url"],
+          urlToImage: element["urlToImage"],
+          publishedAt: element["publishedAt"],
+          content: element["content"],
+        );
+      }
+    });
+  }
+  }
 }

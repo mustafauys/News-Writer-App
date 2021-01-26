eb7952b6315919418bc704e0fc78fba183506ee5
Mustafa <18701031@mersin.edu.tr>
2021-01-26 23:53:33 +0300

    Variables Added

--- lib/models/article_model.dart
+++ lib/models/article_model.dart
@@ -5,4 +5,7 @@ class ArticleModel {
   String url;
   String urlToImage;
   String content;
+  DateTime publishedAt;
+
+  ArticleModel({this.author, this.title, this.url, this.description, this.content, this.urlToImage, this.publishedAt});
 }

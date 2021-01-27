a5f6f6265a014c45ad148f7bc1a962c4434c6462
Mustafa <18701031@mersin.edu.tr>
2021-01-27 23:43:00 +0300

    Language Updated

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -6,7 +6,7 @@ import 'package:http/http.dart' as http;
 class News {
   List<ArticleModel> news = [];
   Future<void> getNews() async {
-    String url = "http://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
+    String url = "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
 
   var response = await http.get(url);
   var jsonData = jsonDecode(response.body);
@@ -14,13 +14,13 @@ class News {
     jsonData["articles"].forEach((element){
 
       if(element["urlToImage"] != null && element['description'] !=null) {
+        // ignore: unused_local_variable
         ArticleModel articleModel = ArticleModel(
           title: element['title'],
           author: element["author"],
           description: element["description"],
           url: element["url"],
           urlToImage: element["urlToImage"],
-          publishedAt: element["publishedAt"],
           content: element["content"],
         );
       }

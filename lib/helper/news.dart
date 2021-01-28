920ee8c7cc9032b8f2b206f450a58bdcdbcc98b6
Mustafa <18701031@mersin.edu.tr>
2021-01-28 23:41:51 +0300

    Switching Between Categories Solved

--- lib/helper/news.dart
+++ lib/helper/news.dart
@@ -4,9 +4,10 @@ import 'package:flutternews/models/article_model.dart';
 import 'package:http/http.dart' as http;
 
 class News {
+  
   List<ArticleModel> news = [];
   Future<void> getNews() async {
-    String url = "http://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=97322a16fa4e474797dfd4f8eece3371";
+    String url = "http://newsapi.org/v2/top-headlines?country=in&apiKey=97322a16fa4e474797dfd4f8eece3371";
 
   var response = await http.get(url);
   var jsonData = jsonDecode(response.body);
@@ -28,3 +29,31 @@ class News {
   }
   }
 }
+
+class CategoryNewsClass {
+  
+  List<ArticleModel> news = [];
+  Future<void> getNews(String category) async {
+    String url = "http://newsapi.org/v2/top-headlines?country=in&category=$category&country=in&apiKey=97322a16fa4e474797dfd4f8eece3371";
+
+  var response = await http.get(url);
+  var jsonData = jsonDecode(response.body);
+  if(jsonData['status'] == "ok") {
+    jsonData["articles"].forEach((element){
+
+      if(element["urlToImage"] != null && element['description'] !=null) {
+        // ignore: unused_local_variable
+        ArticleModel articleModel = ArticleModel(
+          title: element['title'],
+          author: element["author"],
+          description: element["description"],
+          url: element["url"],
+          urlToImage: element["urlToImage"],
+          content: element["content"],
+        );
+      }
+    });
+  }
+  }
+}
+

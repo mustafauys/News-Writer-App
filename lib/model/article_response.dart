6954a8dc90ff545b7f1f3c29ed9f710b90b39e06
Mustafa <18701031@mersin.edu.tr>
2021-01-29 23:55:46 +0300

    Article Response File Added

---
+++ lib/model/article_response.dart
@@ -0,0 +1,18 @@
+import 'article.dart';
+
+class ArticleResponse {
+  final List<ArticleModel> articles;
+  final String error;
+
+  ArticleResponse(this.articles, this.error);
+
+  ArticleResponse.fromJson(Map<String, dynamic> json)
+      : articles = (json["articles"] as List)
+            .map((i) => new ArticleModel.fromJson(i))
+            .toList(),
+        error = "";
+
+  ArticleResponse.withError(String errorValue)
+      : articles = List(),
+        error = errorValue;
+}

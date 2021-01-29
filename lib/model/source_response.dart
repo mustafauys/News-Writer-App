d817c1a845a2c6fdaa167f4c3f8631edc4be2743
Mustafa <18701031@mersin.edu.tr>
2021-01-29 23:55:30 +0300

    Source Response File Added

---
+++ lib/model/source_response.dart
@@ -0,0 +1,18 @@
+import 'source.dart';
+
+class SourceResponse {
+  final List<SourceModel> sources;
+  final String error;
+
+  SourceResponse(this.sources, this.error);
+
+  SourceResponse.fromJson(Map<String, dynamic> json)
+      : sources = (json["sources"] as List)
+            .map((i) => new SourceModel.fromJson(i))
+            .toList(),
+        error = "";
+
+  SourceResponse.withError(String errorValue)
+      : sources = List(),
+        error = errorValue;
+}

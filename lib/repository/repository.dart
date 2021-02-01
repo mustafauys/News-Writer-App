f0242a8f5ba63fed8fd74d3472828df1a990af56
Mustafa <18701031@mersin.edu.tr>
2021-02-01 23:52:17 +0300

    Database Keys Updated

--- lib/repository/repository.dart
+++ lib/repository/repository.dart
@@ -1,88 +1,81 @@
+import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:newsapp/model/article_response.dart';
+
 import 'package:newsapp/model/source_response.dart';
 
 class NewsRepository {
   static String mainUrl = "https://newsapi.org/v2/";
-  final String apiKey = "97322a16fa4e474797dfd4f8eece3371";
-
+  final String apiKey = "3c8673ddff2f42eb81b5dfe63aec7be0";
+  
   final Dio _dio = Dio();
 
-  var getSourcesUrl = "$mainUrl/sources";
-  var getTopHeadlinesUrl = "$mainUrl/top-headlines";
+  var getSourcesUrl = '$mainUrl/sources';
+  var getTopHeadlinesUrl = '$mainUrl/top-headlines';
   var everythingUrl = "$mainUrl/everything";
-
+  
   Future<SourceResponse> getSources() async {
     var params = {
-      "apiKey" : apiKey,
+      "apiKey": apiKey,
       "language" : "en",
-      "country" : "us",
-    };
-
-    try{
+      "country" : "us"};
+    try {
       Response response = await _dio.get(getSourcesUrl, queryParameters: params);
       return SourceResponse.fromJson(response.data);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return SourceResponse.withError("$error");
     }
-     catch (error, stacktrace) {
-       print("Exception occured : $error stackTrace: $stacktrace");
-       return SourceResponse.withError(error);
-     }
   }
-
   Future<ArticleResponse> getTopHeadlines() async {
     var params = {
-      "apiKey" : apiKey,
-      "country" : "us",
-    };
-
+      "apiKey": apiKey,
+      "country" : "us"};
     try {
       Response response = await _dio.get(getTopHeadlinesUrl, queryParameters: params);
       return ArticleResponse.fromJson(response.data);
-    } catch (error){
-      return ArticleResponse.withError(error);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return ArticleResponse.withError("$error");
     }
   }
-
-  Future<ArticleResponse> getHotNews() async {
+  Future<ArticleResponse> search(String value) async {
     var params = {
-      "apiKey" : apiKey,
-      "q" : "apple",
-      "sortBy" : "popularity"
-    };
-
+      "apiKey": apiKey,
+      "q" : value,
+      "sortBy": "popularity"};
     try {
       Response response = await _dio.get(everythingUrl, queryParameters: params);
       return ArticleResponse.fromJson(response.data);
-    } catch (error){
-      return ArticleResponse.withError(error);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return ArticleResponse.withError("$error");
     }
   }
-
+  Future<ArticleResponse> getHotNews() async {
+    var params = {
+      "apiKey": apiKey,
+      "q" : "apple",
+      "sortBy": "popularity"};
+    try {
+      Response response = await _dio.get(everythingUrl, queryParameters: params);
+      return ArticleResponse.fromJson(response.data);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return ArticleResponse.withError("$error");
+    }
+  }
   Future<ArticleResponse> getSourceNews(String sourceId) async {
     var params = {
-      "apiKey" : apiKey,
-      "sources" : sourceId
-    };
-
+      "apiKey": apiKey,
+      "sources" : sourceId};
     try {
       Response response = await _dio.get(getTopHeadlinesUrl, queryParameters: params);
       return ArticleResponse.fromJson(response.data);
-    } catch (error){
-      return ArticleResponse.withError(error);
+    } catch (error, stacktrace) {
+      print("Exception occured: $error stackTrace: $stacktrace");
+      return ArticleResponse.withError("$error");
     }
   }
-
-  Future<ArticleResponse> search(String searchValue) async {
-    var params = {
-      "apiKey" : apiKey,
-      "q" : searchValue
-    };
-
-    try {
-      Response response = await _dio.get(getTopHeadlinesUrl, queryParameters: params);
-      return ArticleResponse.fromJson(response.data);
-    } catch (error){
-      return ArticleResponse.withError(error);
-    }
-  }
+  
 }

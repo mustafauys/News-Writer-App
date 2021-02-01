f3c4e3bffd1fd25acbd6ab2b65aca060fbcc36c8
Mustafa <18701031@mersin.edu.tr>
2021-02-01 23:53:38 +0300

    Edited

--- lib/bloc/bottom_navbar_bloc.dart
+++ lib/bloc/bottom_navbar_bloc.dart
@@ -1,27 +1,26 @@
-
 import 'dart:async';
 
-enum NavBarItem {HOME, SOURCES, SEARCH}
+enum NavBarItem { HOME, SOURCES, SEARCH }
 
 class BottomNavBarBloc {
   final StreamController<NavBarItem> _navBarController =
-        StreamController<NavBarItem>.broadcast();
+      StreamController<NavBarItem>.broadcast();
 
   NavBarItem defaultItem = NavBarItem.HOME;
 
   Stream<NavBarItem> get itemStream => _navBarController.stream;
 
   void pickItem(int i) {
-    switch(i) {
+    switch (i) {
       case 0:
-      _navBarController.sink.add(NavBarItem.HOME);
-      break;
+        _navBarController.sink.add(NavBarItem.HOME);
+        break;
       case 1:
-      _navBarController.sink.add(NavBarItem.SOURCES);
-      break;
+        _navBarController.sink.add(NavBarItem.SOURCES);
+        break;
       case 2:
-      _navBarController.sink.add(NavBarItem.SEARCH);
-      break;
+        _navBarController.sink.add(NavBarItem.SEARCH);
+        break;
     }
   }
 

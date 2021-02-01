9688faf732e4ffd8ad2ce15f4f68dec0c61ace02
Mustafa <18701031@mersin.edu.tr>
2021-02-01 23:52:42 +0300

    Json Updated

--- lib/model/article_response.dart
+++ lib/model/article_response.dart
@@ -1,4 +1,4 @@
-import 'article.dart';
+import 'package:newsapp/model/article.dart';
 
 class ArticleResponse {
   final List<ArticleModel> articles;
@@ -7,9 +7,8 @@ class ArticleResponse {
   ArticleResponse(this.articles, this.error);
 
   ArticleResponse.fromJson(Map<String, dynamic> json)
-      : articles = (json["articles"] as List)
-            .map((i) => new ArticleModel.fromJson(i))
-            .toList(),
+      : articles =
+            (json["articles"] as List).map((i) => new ArticleModel.fromJson(i)).toList(),
         error = "";
 
   ArticleResponse.withError(String errorValue)

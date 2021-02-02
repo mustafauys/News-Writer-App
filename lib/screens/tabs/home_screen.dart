d8a404b0c2939e5ced09d0910ab787154f4a8490
Mustafa <18701031@mersin.edu.tr>
2021-02-02 23:16:01 +0300

    Home Screen Edited

--- lib/screens/tabs/home_screen.dart
+++ lib/screens/tabs/home_screen.dart
@@ -17,27 +17,31 @@ class _HomeScreenState extends State<HomeScreen> {
         Padding(
           padding: const EdgeInsets.all(10.0),
           child: Row(
-              children: <Widget>[
-                Text("Top channels", style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17.0
-                ),),  
-              ],
-            ),
+            children: <Widget>[
+              Text(
+                "Top channels",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17.0),
+              ),
+            ],
+          ),
         ),
         TopChannelsWidget(),
         Padding(
           padding: const EdgeInsets.all(10.0),
           child: Row(
-              children: <Widget>[
-                Text("Hot news", style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 17.0
-                ),),  
-              ],
-            ),
+            children: <Widget>[
+              Text(
+                "Hot news",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 17.0),
+              ),
+            ],
+          ),
         ),
         HotNewsWidget()
       ],

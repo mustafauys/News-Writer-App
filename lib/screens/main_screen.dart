434bbb3d583cbb8e629d117d7813857e0d0650e2
Mustafa <18701031@mersin.edu.tr>
2021-02-02 23:15:11 +0300

    Main Screen Edited

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,8 +1,9 @@
 import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
 import 'package:newsapp/bloc/bottom_navbar_bloc.dart';
+import 'package:newsapp/screens/tabs/search_screen.dart';
 import 'package:newsapp/style/theme.dart' as Style;
-
 import 'tabs/home_screen.dart';
 import 'tabs/search_screen.dart';
 import 'tabs/sources_screen.dart';
@@ -19,21 +20,25 @@ class _MainScreenState extends State<MainScreen> {
     super.initState();
     _bottomNavBarBloc = BottomNavBarBloc();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: HexColor("374760"),
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(50.0),
         child: AppBar(
-          backgroundColor: Style.Colors.mainColor,
-          title: Text("NewsApp", style: TextStyle(
-            color: Colors.white
-          ),),
+          backgroundColor: HexColor("1a1a2e"),
+          title: Text(
+            "Did You Read The News",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
       body: SafeArea(
-              child: StreamBuilder<NavBarItem>(
+        child: StreamBuilder<NavBarItem>(
           stream: _bottomNavBarBloc.itemStream,
           initialData: _bottomNavBarBloc.defaultItem,
           // ignore: missing_return
@@ -55,19 +60,20 @@ class _MainScreenState extends State<MainScreen> {
         builder: (BuildContext context, AsyncSnapshot<NavBarItem> snapshot) {
           return Container(
             decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
-          boxShadow: [
-            BoxShadow(color: Colors.grey[100], spreadRadius: 0, blurRadius: 10),
-          ],
-        ),
+              borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.white, spreadRadius: 0, blurRadius: 10),
+              ],
+            ),
             child: ClipRRect(
               borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(30.0),
-              topRight: Radius.circular(30.0),
-            ),
-                        child: BottomNavigationBar(
-                backgroundColor: Colors.white,
+                topLeft: Radius.circular(30.0),
+                topRight: Radius.circular(30.0),
+              ),
+              child: BottomNavigationBar(
+                backgroundColor: HexColor("1a1a2e"),
                 iconSize: 20,
                 unselectedItemColor: Style.Colors.grey,
                 unselectedFontSize: 9.5,
@@ -78,29 +84,32 @@ class _MainScreenState extends State<MainScreen> {
                 onTap: _bottomNavBarBloc.pickItem,
                 items: [
                   BottomNavigationBarItem(
-                      // ignore: deprecated_member_use
-                      title: Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Text("Home",
-                              style: TextStyle(fontWeight: FontWeight.w600))),
-                      icon: Icon(EvaIcons.homeOutline),
-                      activeIcon: Icon(EvaIcons.home),),
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Text("Home",
+                            style: TextStyle(fontWeight: FontWeight.w600))),
+                    icon: Icon(EvaIcons.homeOutline),
+                    activeIcon: Icon(EvaIcons.home),
+                  ),
                   BottomNavigationBarItem(
-                      // ignore: deprecated_member_use
-                      title: Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Text("Sources",
-                              style: TextStyle(fontWeight: FontWeight.w600))),
-                      icon: Icon(EvaIcons.gridOutline),
-                      activeIcon: Icon(EvaIcons.grid),),
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Text("Sources",
+                            style: TextStyle(fontWeight: FontWeight.w600))),
+                    icon: Icon(EvaIcons.gridOutline),
+                    activeIcon: Icon(EvaIcons.grid),
+                  ),
                   BottomNavigationBarItem(
-                      // ignore: deprecated_member_use
-                      title: Padding(
-                          padding: EdgeInsets.only(top: 5.0),
-                          child: Text("Search",
-                              style: TextStyle(fontWeight: FontWeight.w600))),
-                      icon: Icon(EvaIcons.searchOutline),
-                      activeIcon: Icon(EvaIcons.search),),
+                    // ignore: deprecated_member_use
+                    title: Padding(
+                        padding: EdgeInsets.only(top: 5.0),
+                        child: Text("Search",
+                            style: TextStyle(fontWeight: FontWeight.w600))),
+                    icon: Icon(EvaIcons.searchOutline),
+                    activeIcon: Icon(EvaIcons.search),
+                  ),
                 ],
               ),
             ),
@@ -109,6 +118,7 @@ class _MainScreenState extends State<MainScreen> {
       ),
     );
   }
+
   Widget testScreen() {
     return Container(
       color: Colors.white,
@@ -116,9 +126,7 @@ class _MainScreenState extends State<MainScreen> {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text("Test Screen")
-        ],
+        children: <Widget>[Text("Test Screen")],
       ),
     );
   }
